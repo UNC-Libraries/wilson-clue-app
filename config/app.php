a833c97cc9d8472479c094e8bf3fc3223187efb3
Shift <shift@laravelshift.com>
2022-08-31 14:14:00 +0000

    Shift config files

--- config/app.php
+++ config/app.php
@@ -194,6 +194,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Gate' => Illuminate\Support\Facades\Gate::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
+        'Http' => Illuminate\Support\Facades\Http::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
