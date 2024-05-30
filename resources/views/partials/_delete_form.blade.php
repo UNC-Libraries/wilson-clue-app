87824766a8824bbd2b59bccd2566e29ff7ff1cbf
lfarrell <lfarrell@email.unc.edu>
2024-05-30 15:28:32 -0400

    Fix merge conflicts between bootstrap 5 and switching out HTML generator

--- resources/views/partials/_delete_form.blade.php
+++ resources/views/partials/_delete_form.blade.php
@@ -1,3 +1,4 @@
+{{ $route }}
 {{ html()->form('DELETE', route($route))->class('confirm-submit float-end')->data('message', empty($message) ? 'Are you sure you want to delete this?' : $message)->open() }}
     <button type="submit" class="btn btn-danger {{ $class ?? 'float-end' }}"><span class="fa fa-trash"></span></button>
 {{ html()->form()->close() }}
