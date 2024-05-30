5478236a840bf1548ef633e63b655846ba270a66
lfarrell <lfarrell@email.unc.edu>
2024-05-30 15:31:53 -0400

    Fix routing issues

--- resources/views/partials/_delete_form.blade.php
+++ resources/views/partials/_delete_form.blade.php
@@ -1,4 +1,6 @@
-{{ $route }}
-{{ html()->form('DELETE', route($route))->class('confirm-submit float-end')->data('message', empty($message) ? 'Are you sure you want to delete this?' : $message)->open() }}
+{{-- See vendor/laravel/framework/src/Illuminate/Foundation/helpers.php for how route() works.
+The first option is a string with the route. The second option is the parameters as an array.
+Parameters come in as a single array so shifting off the first element, which is the route path --}}
+{{ html()->form('DELETE', route(array_shift($route), $route))->class('confirm-submit float-end')->data('message', empty($message) ? 'Are you sure you want to delete this?' : $message)->open() }}
     <button type="submit" class="btn btn-danger {{ $class ?? 'float-end' }}"><span class="fa fa-trash"></span></button>
 {{ html()->form()->close() }}
