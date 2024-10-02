a46ae88ccb9255a2129cfc63e8006505e6928599
lfarrell <lfarrell@email.unc.edu>
2024-10-02 15:55:29 -0400

    Multi-auth

--- app/Player.php
+++ app/Player.php
@@ -137,7 +137,7 @@ class Player extends  Authenticatable implements LdapAuthenticatable
     public function updateFromOnyen($onyen, $override_student = false)
     {
         if ($this->validOnyen($onyen)) {
-            $getPerson = PlayerUser::query()->where('uid', '=', $onyen)->get();
+            $getPerson = PlayerUser::query()->whereRaw('uid', '=', $onyen)->get();
             print_r($getPerson); exit;
             $uncPerson = $getPerson->first();
 
