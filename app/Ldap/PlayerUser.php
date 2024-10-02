4e5e49e19e4e8e9f48a4783a97ff448f8499d52d
lfarrell <lfarrell@email.unc.edu>
2024-10-02 14:35:58 -0400

    Multi-auth

--- app/Ldap/PlayerUser.php
+++ app/Ldap/PlayerUser.php
@@ -6,10 +6,5 @@ use LdapRecord\Models\ActiveDirectory\User;
 
 class PlayerUser extends User
 {
-    /**
-     * The object classes of the LDAP model.
-     */
-    public static array $objectClasses = [];
-
     protected ?string $connection = 'people';
 }
