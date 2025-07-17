10e4d40e413428abca4f88b18982ccb28ae884fa
Shift <shift@laravelshift.com>
2025-07-17 14:26:43 +0000

    Re-register service providers

--- config/app.php
+++ config/app.php
@@ -7,25 +7,6 @@ return [
 
     'timezone' => 'America/New_York',
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Laravel Framework Service Providers...
-         */
-
-        /*
-         * Package Service Providers...
-         */
-        Laravel\Tinker\TinkerServiceProvider::class,
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-    ])->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         'ClueValidator' => App\Validation\ClueValidator::class,
