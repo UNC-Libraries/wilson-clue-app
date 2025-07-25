6370abe81d58d48af2c6cff6d8963939195e65c4
lfarrell <lfarrell@email.unc.edu>
2025-07-25 11:58:13 -0400

    Fix issue with json-ld breaking the template

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -17,35 +17,37 @@
 
 @section('additional_scripts')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.1.0/js/swiper.min.js"></script>
-    <script type="application/ld+json">
-        {
-            "@context": "http://schema.org",
-            "@id": "https://library.unc.edu/wilson/",
-            "@type": "Organization",
-                "parentOrganization": {
-                    "@type": "Organization",
-                    "name": "UNC Chapel Hill Libraries",
-                    "parentOrganization": {
-                        "@type": "CollegeOrUniversity",
-                        "name": "University of North Carolina at Chapel Hill"
-                    }
-                },
-                "logo": "https://library.unc.edu/wp-content/themes/responsiveUNCLib/images/wilsonbanner.png",
-                "url": "https://library.unc.edu/wilson/",
-                "name": "Louis Round Wilson Library",
-                "sameAs": "https://en.wikipedia.org/wiki/Louis_Round_Wilson_Library"
-        }
-    </script>
-    <script type="application/ld+json">
-        {
-            "@context": "https://schema.org",
-            "@id": "homepage",
-            "@type": "WebPage",
-            "keywords": "wilson,library,clue,unc",
-            "publisher":
+    @verbatim
+        <script type="application/ld+json">
             {
-                "@id":"http://library.unc.edu/wilson/"
+                "@context": "http://schema.org",
+                "@id": "https://library.unc.edu/wilson/",
+                "@type": "Organization",
+                    "parentOrganization": {
+                        "@type": "Organization",
+                        "name": "UNC Chapel Hill Libraries",
+                        "parentOrganization": {
+                            "@type": "CollegeOrUniversity",
+                            "name": "University of North Carolina at Chapel Hill"
+                        }
+                    },
+                    "logo": "https://library.unc.edu/wp-content/themes/responsiveUNCLib/images/wilsonbanner.png",
+                    "url": "https://library.unc.edu/wilson/",
+                    "name": "Louis Round Wilson Library",
+                    "sameAs": "https://en.wikipedia.org/wiki/Louis_Round_Wilson_Library"
             }
-        }
-    </script>
+        </script>
+        <script type="application/ld+json">
+            {
+                "@context": "https://schema.org",
+                "@id": "homepage",
+                "@type": "WebPage",
+                "keywords": "wilson,library,clue,unc",
+                "publisher":
+                {
+                    "@id":"http://library.unc.edu/wilson/"
+                }
+            }
+        </script>
+    @endverbatim
 @endsection
