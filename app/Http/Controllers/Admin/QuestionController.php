ddbb01f4c5b810bc4beaf733a018254e9ebfdc82
lfarrell <lfarrell@email.unc.edu>
2025-10-24 11:41:43 -0400

    testing

--- app/Http/Controllers/Admin/QuestionController.php
+++ app/Http/Controllers/Admin/QuestionController.php
@@ -160,7 +160,8 @@ class QuestionController extends Controller
         Log::error("Image: ". $request->hasFile('new_image_file'));
         $question->type = $request->type ? 1 : 0;
         // Update Image
-        Log::error("Image: ". $request->file('new_image_file')->getErrorMessage());
+        Log::error("Imagez: ". $request->new_image_file);
+        Log::error("Image: ". $request->file('new_image_file'));
         if ($request->hasFile('new_image_file')) {
             $this->validate($request, [
                 'new_image_file' => 'max:1024|mimetypes:image/jpeg,image/png,image/svg+xml',
