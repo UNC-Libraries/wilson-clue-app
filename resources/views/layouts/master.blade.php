6eef0c6d6a45746f4a747864e969435112c7276c
lfarrell <lfarrell@email.unc.edu>
2026-01-15 15:00:56 -0500

    testing

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -23,6 +23,7 @@ and are used to route JavaScript in app.js --}}
 
     @yield('additional_scripts')
     @vite('resources/assets/js/app.js')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.14.1/jquery-ui.min.js" integrity="sha512-MSOo1aY+3pXCOCdGAYoBZ6YGI0aragoQsg1mKKBHXCYPIWxamwOE7Drh+N5CPgGI5SA9IEKJiPjdfqWFWmZtRA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
 
 </html>
