57f4fccbda9c6f12b2f676c08f81e802c622768b
lfarrell <lfarrell@email.unc.edu>
2026-03-10 15:28:37 -0400

    Fix test names so phpunit can fine them

--- tests/Unit/Validation/ClueValidatorTest.php
+++ tests/Unit/Validation/ClueValidatorTest.php
@@ -15,7 +15,7 @@ class ClueValidatorTest extends TestCase
     }
 
     
-    public function it_returns_true_when_value_contains_all_required_characters(): void
+    public function test_it_returns_true_when_value_contains_all_required_characters(): void
     {
         $this->assertTrue($this->validator->checkDnaSequence('dna', 'ghst', null, null));
     }
@@ -27,49 +27,49 @@ class ClueValidatorTest extends TestCase
     }
 
     
-    public function it_returns_true_when_value_contains_all_required_characters_in_different_order(): void
+    public function test_it_returns_true_when_value_contains_all_required_characters_in_different_order(): void
     {
         $this->assertTrue($this->validator->checkDnaSequence('dna', 'thsg', null, null));
     }
 
     
-    public function it_returns_false_when_value_is_missing_g(): void
+    public function test_it_returns_false_when_value_is_missing_g(): void
     {
         $this->assertFalse($this->validator->checkDnaSequence('dna', 'hst', null, null));
     }
 
     
-    public function it_returns_false_when_value_is_missing_h(): void
+    public function test_it_returns_false_when_value_is_missing_h(): void
     {
         $this->assertFalse($this->validator->checkDnaSequence('dna', 'gst', null, null));
     }
 
     
-    public function it_returns_false_when_value_is_missing_s(): void
+    public function test_it_returns_false_when_value_is_missing_s(): void
     {
         $this->assertFalse($this->validator->checkDnaSequence('dna', 'ght', null, null));
     }
 
     
-    public function it_returns_false_when_value_is_missing_t(): void
+    public function test_it_returns_false_when_value_is_missing_t(): void
     {
         $this->assertFalse($this->validator->checkDnaSequence('dna', 'ghs', null, null));
     }
 
     
-    public function it_returns_false_when_value_is_empty(): void
+    public function test_it_returns_false_when_value_is_empty(): void
     {
         $this->assertFalse($this->validator->checkDnaSequence('dna', '', null, null));
     }
 
     
-    public function it_returns_false_when_value_contains_only_one_character(): void
+    public function test_it_returns_false_when_value_contains_only_one_character(): void
     {
         $this->assertFalse($this->validator->checkDnaSequence('dna', 'g', null, null));
     }
 
     
-    public function it_returns_true_when_value_contains_extra_characters_alongside_required_ones(): void
+    public function test_it_returns_true_when_value_contains_extra_characters_alongside_required_ones(): void
     {
         $this->assertTrue($this->validator->checkDnaSequence('dna', 'ghstabc', null, null));
     }
